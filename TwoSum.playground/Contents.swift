b6f5cb9cb9c01906b89df9ec52213208d16040cd
Piotr <piotr.sztefko@iteo.com>
2023-12-23 13:56:07 +0100

    solution

--- TwoSum.playground/Contents.swift
+++ TwoSum.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var dict = [Int: Int]()
+
+    for (index, value) in nums.enumerated() {
+
+        if let addent = dict[value] {
+            return [addent, index]
+        } else {
+            dict[target - value] = index
+        }
+    }
+    return []
+}
+
+print(twoSum([2,7, 20,11,15], 9))
