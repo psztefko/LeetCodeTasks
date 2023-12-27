0b065453048f5aecbf967445c3d527e0fbbaff77
Piotr <piotr.sztefko@iteo.com>
2023-12-27 16:00:48 +0100

    initial concept

--- Solution.playground/Contents.swift
+++ Solution.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+func longestCommonPrefix(_ strings: [String]) -> String {
+    var commonPrefix = ""
+    var flag = true
+
+    strings[0].forEach { char in
+        var char = String(char)
+        if strings.allSatisfy { $0.hasPrefix(char) } {
+            commonPrefix += char
+        } else {
+            flag = false
+        }
+    }
+
+    return commonPrefix
+}
+
+longestCommonPrefix(["flower","flow","flight"])
