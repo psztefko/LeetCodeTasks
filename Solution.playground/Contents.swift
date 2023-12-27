42b1a7edc104ee5936582219dcd4d5e72fb6d660
Piotr <piotr.sztefko@iteo.com>
2023-12-27 14:04:05 +0100

    solved with complexity O(n)

--- Solution.playground/Contents.swift
+++ Solution.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let symbolsDict: [Character:Int] = [
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+]
+
+func romanToInt(_ s: String) -> Int {
+    var finalInt = 0, previousValue = 0
+
+    for char in s {
+        if let currentValue = symbolsDict[char] {
+            finalInt += currentValue <= previousValue ? previousValue : -previousValue
+            previousValue = currentValue
+        }
+    }
+    finalInt += previousValue
+
+    return finalInt
+}
