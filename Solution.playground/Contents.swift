d9144130f5b62374b51c4ee1d3d067966e140161
Piotr <piotr.sztefko@iteo.com>
2024-01-02 15:35:05 +0100

    first approach

--- Solution.playground/Contents.swift
+++ Solution.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+    var temp: [Int] = []
+    var indexes: [Int] = []
+
+    for (index, num) in nums.enumerated() {
+        if temp.contains(num) {
+            indexes.insert(index, at: 0)
+        } else {
+            temp.append(num)
+        }
+    }
+    for index in indexes {
+        nums.remove(at: index)
+    }
+    return nums.count
+}
+
+var nums = [0,0,1,1,1,2,2,3,3,4]
+removeDuplicates(&nums)
