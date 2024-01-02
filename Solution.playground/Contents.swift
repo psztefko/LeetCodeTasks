76efaca92b98ff8e45d2a6dddb04504b1db5ef2d
Piotr <piotr.sztefko@iteo.com>
2024-01-02 12:16:08 +0100

    simplified approach

--- Solution.playground/Contents.swift
+++ Solution.playground/Contents.swift
@@ -2,17 +2,15 @@ import UIKit
 
 func isValid(_ s: String) -> Bool {
     guard s.count % 2 == 0 else { return false }
-    var stack: [Character] = []
+    var stack = [Character]()
+
+    let dict: [Character: Character] = ["(": ")", "{": "}", "[": "]"]
 
     for char in s {
         switch char {
-        case "(": stack.append(")")
-        case "[": stack.append("]")
-        case "{": stack.append("}")
-        default:
-            if stack.isEmpty || stack.removeLast() != char {
-                return false
-            }
+        case "(", "{", "[": stack.append(char)
+        case ")", "}", "]": guard !stack.isEmpty, dict[stack.removeLast()] == char else { return false }
+        default: return false
         }
     }
     return stack.isEmpty
