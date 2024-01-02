74756769cef285e5a1f8ede280c18264f8feb1ee
Piotr <piotr.sztefko@iteo.com>
2024-01-02 16:29:11 +0100

    simplified solution

--- Solution.playground/Contents.swift
+++ Solution.playground/Contents.swift
@@ -1,21 +1,6 @@
 import UIKit
 
 func removeDuplicates(_ nums: inout [Int]) -> Int {
-    var temp: [Int] = []
-    var indexes: [Int] = []
-
-    for (index, num) in nums.enumerated() {
-        if temp.contains(num) {
-            indexes.insert(index, at: 0)
-        } else {
-            temp.append(num)
-        }
-    }
-    for index in indexes {
-        nums.remove(at: index)
-    }
+    nums = Array(Set(nums)).sorted()
     return nums.count
 }
-
-var nums = [0,0,1,1,1,2,2,3,3,4]
-removeDuplicates(&nums)
