00e3e026d7f6260176597a17f08880af8fcd0c37
Piotr <piotr.sztefko@iteo.com>
2024-01-02 09:32:39 +0100

    cleanup

--- Solution.playground/Contents.swift
+++ Solution.playground/Contents.swift
@@ -2,12 +2,10 @@ import UIKit
 
 
 func longestCommonPrefix(_ strings: [String]) -> String {
-    guard let firstString = strings.first, !firstString.isEmpty else {
-        return ""
-    }
-
-    return strings.dropFirst().reduce(firstString) { commonPrefix, nextString in
-        String(zip(commonPrefix, nextString).prefix(while: { $0.0 == $0.1 }).map { $0.0 })
+    return strings.lazy.reduce(strings[0]) { commonPrefix, nextString in
+        String(zip(commonPrefix, nextString).prefix(while: {
+            $0.0 == $0.1
+        }).map { $0.0 })
     }
 }
 
