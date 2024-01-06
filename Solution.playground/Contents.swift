e79a0e04159e423516313bc9d19c659dab41143f
Piotr <piotr.sztefko@iteo.com>
2024-01-06 20:06:26 +0100

    solution

--- Solution.playground/Contents.swift
+++ Solution.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func mergeAlternately(_ word1: String, _ word2: String) -> String {
+    var merged = ""
+
+    for (char1, char2) in zip(word1, word2) {
+        merged.append(char1)
+        merged.append(char2)
+    }
+
+    if word1.count < word2.count {
+        merged += word2[word1.endIndex...]
+    } else {
+        merged += word1[word2.endIndex...]
+    }
+
+    return merged
+}
