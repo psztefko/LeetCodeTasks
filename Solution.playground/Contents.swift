9b1c8d2fcb09c5f4e5434aaeb5dc5d243e58a234
Piotr <piotr.sztefko@iteo.com>
2024-01-07 13:35:04 +0100

    conver strings to arrays and distinct longer and shorter

--- Solution.playground/Contents.swift
+++ Solution.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func gcdOfStrings(_ str1: String, _ str2: String) -> String {
+    var short: [String.Element]
+    var long: [String.Element]
+
+    if str1.count <= str2.count {
+        short = Array(str1)
+        long = Array(str2)
+    } else {
+        short = Array(str2)
+        long = Array(str1)
+    }
+
+    return ""
+}
+
+gcdOfStrings("ABCABC", "ABC")
