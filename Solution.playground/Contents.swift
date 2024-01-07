75a63c57c0aa75b3a71174ef378de4f70f6defa6
Piotr <piotr.sztefko@iteo.com>
2024-01-07 15:00:59 +0100

    solution

--- Solution.playground/Contents.swift
+++ Solution.playground/Contents.swift
@@ -1,44 +1,16 @@
 import UIKit
 
 func gcdOfStrings(_ str1: String, _ str2: String) -> String {
-    var short: [String.Element]
-    var long: [String.Element]
+    guard (str1 + str2) == (str2 + str1) else { return "" }
 
-    if str1.count <= str2.count {
-        short = Array(str1)
-        long = Array(str2)
-    } else {
-        short = Array(str2)
-        long = Array(str1)
-    }
-
-    var longestSubsequence = ""
-    var currentLongestSubsequence = ""
-
-    for i in 0...long.count - 1 {
-        var sequenceStreak = true
-        for j in 0...short.count - 1 {
-            if i + j < long.count {
-                if long[i + j] == short[j] {
-                    if sequenceStreak {
-                        currentLongestSubsequence.append(short[j])
-                    }
-
-                } else {
-                    sequenceStreak = false
-                }
-            }
-            if currentLongestSubsequence.count == short.count {
-                return currentLongestSubsequence
-            }
-
-            if currentLongestSubsequence.count > longestSubsequence.count {
-                longestSubsequence = currentLongestSubsequence
-            }
-        }
-    }
-
-    return longestSubsequence
+    let gcd = gcd(str1.count, str2.count)
+    return String(str1.prefix(gcd))
 }
 
-gcdOfStrings("ABCABC", "ABC")
+func gcd(_ x: Int, _ y: Int) -> Int {
+    if y == 0 {
+        return x;
+    } else {
+        return gcd(y, x % y);
+    }
+}
