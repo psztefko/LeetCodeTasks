be72d79e59636d980fc8a7c086475d87e9bf02e1
Piotr <piotr.sztefko@iteo.com>
2024-01-10 07:08:17 +0100

    change playground to command line tool for better debugging experience

--- Solution.playground/Contents.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-
-func kidsWithCandies(_ candies: [Int], _ extraCandies: Int) -> [Bool] {
-    return candies.compactMap { $0 + extraCandies >= candies.max()! }
-}
---
+++ Solution/Solution/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Solution
+//
+//  Created by Piotr Sztefko on 10/01/2024.
+//
+
+import Foundation
+
+print("Hello, World!")
+
