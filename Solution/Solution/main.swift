6b230065966f2eef3bf09a2ad8e127a30d1fe933
Piotr <piotr.sztefko@iteo.com>
2024-01-10 08:34:47 +0100

    working solution

--- Solution/Solution/main.swift
+++ Solution/Solution/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
+    var flowerbed = flowerbed
+    var n = n
+    var i = 1
 
+    flowerbed.insert(0, at: 0)
+    flowerbed.insert(0, at: flowerbed.count)
+
+    while (i < flowerbed.count - 1) && n > 0 {
+        if (flowerbed[i - 1] == 0) && (flowerbed[i] == 0) && (flowerbed[i + 1] == 0) {
+            flowerbed[i] = 1
+            n -= 1
+        }
+        i += 1
+    }
+
+    return (n == 0 ? true : false)
+}
