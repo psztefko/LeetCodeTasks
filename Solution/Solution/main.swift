c68a9e58da47dd7a03a16893a7b6b065786fc6b6
Piotr <piotr.sztefko@iteo.com>
2024-01-12 21:38:23 +0100

    first working solution

--- Solution/Solution/main.swift
+++ Solution/Solution/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
+let vowels: [Character] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
+func reverseVowels(_ s: String) -> String {
+    var vowelsInString: [Character] = []
+    var vowelsIndexes: [Int] = []
+    var result = s.map { $0 }
+
+    for (index, char) in s.enumerated() {
+        if vowels.contains(char) {
+            vowelsInString.append(char)
+            vowelsIndexes.append(index)
+        }
+    }
+
+    for (i, vowel) in vowelsIndexes.enumerated() {
+        result[vowelsIndexes[i]] = vowelsInString.reversed()[i]
+    }
+
+    return String(result)
+}
+
+print(reverseVowels("aA"))
