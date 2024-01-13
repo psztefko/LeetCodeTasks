817f55da145ff84896900e78429819df305904ef
Piotr <piotr.sztefko@iteo.com>
2024-01-13 21:16:25 +0100

    initial solution with bad complexity

--- Solution/Solution/main.swift
+++ Solution/Solution/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
-print("Hello, World!")
+func productExceptSelf(_ nums: [Int]) -> [Int] {
+    var result: [Int] = []
 
+    for index in 0..<nums.count {
+        var tempNums = nums
+        var temp = tempNums.remove(at: index)
+        result.append(tempNums.reduce(1) { $0 * $1 })
+    }
+
+    return result
+}
+
+//var nums = [1,2,3,4]
+var nums = [-1,1,0,-3,3]
+print(productExceptSelf(nums))
