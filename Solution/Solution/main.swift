0d032c1836274b51d88796d5c375c51f7bac3ab7
Piotr <piotr.sztefko@iteo.com>
2024-01-15 11:11:36 +0100

    triplet numbers comes one after another

--- Solution/Solution/main.swift
+++ Solution/Solution/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+func increasingTriplet(_ nums: [Int]) -> Bool {
+    var i = 1
+    var previousValue = nums[0]
+    var sequenceLength = 1
 
+    while sequenceLength < 3 && i < nums.count {
+        if nums[i] > previousValue {
+            sequenceLength += 1
+        } else {
+            sequenceLength = 1
+        }
+        previousValue = nums[i]
+        i += 1
+    }
+
+    return (sequenceLength == 3) ? true : false
+}
+
+var nums = [2,1,5,0,4,6]
+//var nums = [5,4,3,2,1]
+print(increasingTriplet(nums))
