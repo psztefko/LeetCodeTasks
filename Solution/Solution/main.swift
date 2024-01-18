1eb93d5e50cdc4271bf9b500f193e097d1a23c3d
Piotr <piotr.sztefko@iteo.com>
2024-01-18 13:10:19 +0100

    proper solution

--- Solution/Solution/main.swift
+++ Solution/Solution/main.swift
@@ -8,31 +8,18 @@
 import Foundation
 
 func compress(_ chars: inout [Character]) -> Int {
-    var tempArray: [Character] = []
-    var currentChar: Character = chars[0]
-    var currentCount: Int = 0
+    var tempString = ""
+    var currentCount = 1
 
-    for char in chars {
-        if char != currentChar && currentCount > 0 {
-            tempArray.append(currentChar)
-            if currentCount > 1 {
-                tempArray.append(Character(String((currentCount))))
-            }
-            currentChar = char
-            currentCount = 1
-        } else {
+    for i in 1..<chars.count {
+        if chars[i] == chars[i - 1] {
             currentCount += 1
+        } else {
+            tempString += "\(chars[i - 1])" + (currentCount > 1 ? "\(currentCount)" : "")
+            currentCount = 1
         }
     }
-    tempArray.append(currentChar)
-    tempArray.append(Character(String((currentCount))))
-
-    chars = tempArray
+    tempString += "\(chars.last!)" + (currentCount > 1 ? "\(currentCount)" : "")
+    chars = Array(tempString)
     return chars.count
 }
-
-var chars: [Character] = ["a","a","b","b","c","c","c"]
-compress(&chars)
-print(chars)
-//Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
-//Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
