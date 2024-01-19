e40febee83ae3160218ca3e446d97a826e3fc268
Piotr <piotr.sztefko@iteo.com>
2024-01-19 14:21:27 +0100

    solution

--- Solution/Solution/main.swift
+++ Solution/Solution/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
+func isSubsequence(_ s: String, _ t: String) -> Bool {
+    if s == "" && t == "" {
+        return true
+    } else if t == "" {
+        return false
+    } else if s == "" {
+        return true
+    }
 
+    var s = s.map { $0 }
+    var t = t.map { $0 }
+
+    for char in t {
+        if char == s[0] {
+            s.removeFirst()
+            if s.isEmpty {
+                return true
+            }
+        }
+    }
+    return false
+}
